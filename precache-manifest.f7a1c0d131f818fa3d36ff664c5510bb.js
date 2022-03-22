723c3312c82e51f25860f3b7227bc1a5c05a7edb
maticape <maticape@gmail.com>
2022-03-21 21:54:28 -0700

    update

---
+++ precache-manifest.f7a1c0d131f818fa3d36ff664c5510bb.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bc81942d17e538afe22",
+    "url": "/static/css/main.28e15bff.chunk.css"
+  },
+  {
+    "revision": "9bc81942d17e538afe22",
+    "url": "/static/js/main.9bc81942.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "af3a34228cb436791c7c",
+    "url": "/static/css/2.5566d82c.chunk.css"
+  },
+  {
+    "revision": "af3a34228cb436791c7c",
+    "url": "/static/js/2.af3a3422.chunk.js"
+  },
+  {
+    "revision": "1ee4a1ab4dd95100e8d21e67acf53849",
+    "url": "/static/media/MaticLogo.1ee4a1ab.png"
+  },
+  {
+    "revision": "913bdc3a8850701572719fa24d13c5e6",
+    "url": "/static/media/apeLogo.913bdc3a.png"
+  },
+  {
+    "revision": "fa32082088e7edcf7dab3c1f261e4647",
+    "url": "/static/media/01.fa320820.svg"
+  },
+  {
+    "revision": "16897841f60082408136d267290d625c",
+    "url": "/static/media/logo.16897841.png"
+  },
+  {
+    "revision": "5d516decc4194525f1a2d0792813f85d",
+    "url": "/static/media/main.5d516dec.jpg"
+  },
+  {
+    "revision": "1ef5d830708c04da815ad2dff8ebac4f",
+    "url": "/static/media/05.1ef5d830.svg"
+  },
+  {
+    "revision": "b6b3efc541dc86f3dcf8451b8dedd6a8",
+    "url": "/static/media/06.b6b3efc5.svg"
+  },
+  {
+    "revision": "9a4c17ffeccc7493f450f0ee761e5365",
+    "url": "/static/media/07.9a4c17ff.svg"
+  },
+  {
+    "revision": "2c08ab236ae31418cacab1c49751a57f",
+    "url": "/static/media/08.2c08ab23.svg"
+  },
+  {
+    "revision": "8dfd6c8da879a7a4985b000c6362a98e",
+    "url": "/static/media/09.8dfd6c8d.svg"
+  },
+  {
+    "revision": "8b8983b6e7152092ce323061e97ba117",
+    "url": "/static/media/10.8b8983b6.svg"
+  },
+  {
+    "revision": "65db3bbee9c84fa07dbe379ad5311b51",
+    "url": "/static/media/03.65db3bbe.svg"
+  },
+  {
+    "revision": "e1a4c94f57f51a8d577cc02e59b2c4fe",
+    "url": "/static/media/04.e1a4c94f.svg"
+  },
+  {
+    "revision": "585b934926611638d83b7618f3ed6ff6",
+    "url": "/static/media/01.585b9349.svg"
+  },
+  {
+    "revision": "f36c548aeac6ae66bca61627134ebe2f",
+    "url": "/static/media/02.f36c548a.svg"
+  },
+  {
+    "revision": "2e2075c50dfecdb006e53168524e72bf",
+    "url": "/static/media/03.2e2075c5.svg"
+  },
+  {
+    "revision": "ac57c91e1175332895d1d7ac433f4e66",
+    "url": "/static/media/04.ac57c91e.svg"
+  },
+  {
+    "revision": "d7ad62f1729722e9d4cd8476b779b2de",
+    "url": "/static/media/05.d7ad62f1.svg"
+  },
+  {
+    "revision": "fcf4168db83314eb290f2a9059326bea",
+    "url": "/static/media/06.fcf4168d.svg"
+  },
+  {
+    "revision": "d1ed6da1687a512991573026d39950a2",
+    "url": "/static/media/mainMobile.d1ed6da1.jpg"
+  },
+  {
+    "revision": "486484fca3ba918361c0cd44a009c512",
+    "url": "/static/media/bg.486484fc.png"
+  },
+  {
+    "revision": "c537a43faf7d315c09d90321b763fbf0",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d29eba77964c297ff6aceae472eb8ac.js"
+  "/precache-manifest.f7a1c0d131f818fa3d36ff664c5510bb.js"
 );
 
 workbox.clientsClaim();
